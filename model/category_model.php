47159e08d770c30db0b4693f8aa6f15fbfb10130
zyang19941115 <yz941115@gmail.com>
2019-03-02 20:51:31 +0800

    set delete function as soft delete

--- model/category_model.php
+++ model/category_model.php
@@ -4,7 +4,7 @@ $table_name = 'categories';
 function get_categories() {
     global $db;
     global $table_name;
-    $query = "SELECT * FROM $table_name ORDER BY categoryID DESC";
+    $query = "SELECT * FROM $table_name where is_deleted = 0 ORDER BY categoryID DESC";
     $statement = $db->prepare($query);
     $statement->execute();
     return $statement;
@@ -12,7 +12,7 @@ function get_categories() {
 
 function get_latest_category_id() {
     global $db;
-    $query = "SELECT category_id FROM categories ORDER BY category_id DESC LIMIT 1";
+    $query = "SELECT category_id FROM categories where is_deleted = 0 ORDER BY category_id DESC LIMIT 1";
     $statement = $db->prepare($query);
     $statement->execute();
     $category_id = $statement->fetch();
@@ -22,7 +22,8 @@ function get_latest_category_id() {
 function get_category_name($category_id) {
     global $db;
     $query = 'SELECT * FROM categories
-              WHERE categoryID = :category_id';
+              WHERE categoryID = :category_id
+              AND is_deleted = 0';
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
     $statement->execute();
@@ -42,10 +43,13 @@ function add_category($name) {
     $statement->closeCursor();
 }
 
+/**
+ * @param $category_id
+ * use soft delete
+ */
 function delete_category($category_id) {
     global $db;
-    $query = 'DELETE FROM categories
-              WHERE categoryID = :category_id';
+    $query = 'UPDATE categories SET is_deleted = 1 WHERE categoryID = :category_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
     $statement->execute();
